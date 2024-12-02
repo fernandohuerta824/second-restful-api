6d56f68c6be7ffea85b8c0c27aea6b4f2ca1dfad
Fernando Huerta <fernando.huerta4313@alumnos.udg.mx>
2024-12-02 08:45:13 -0600

    El endpoint GET /products/:id y PATCH /products/:id estan funcionando

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const Products = require('./../models/Products')
+const { Types } = require('mongoose')
 
 const router = Router()
 
@@ -10,7 +11,7 @@ router.get('/', async (req, res) => {
             products
         })
     } catch(error) {
-        error.status = 500
+        error.status ||= 500
         error.message = 'Something failed'
         next(error)
     }
@@ -32,26 +33,70 @@ router.post('/', async (req, res) => {
             product
         })
     } catch(error) {
-        error.status = 500
+        error.status ||= 500
         error.message = 'Something failed'
         next(error)
     }
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const { id } = req.params
-    res.status(200).json({
-        "message": 'GET /products/:id',
-        "id": id
+
+    if(!Types.ObjectId.isValid(id))
+        return res.status(404).json({
+            "message": 'Product not found',
     })
+
+    const product = await Products.findById(new Types.ObjectId(id))
+
+    if(!product)
+        return res.status(404).json({
+            "message": 'Product not found',
+        })
+
+        res.status(200).json({
+            "message": 'Product found',
+            product
+        })
+
+    
 })
 
-router.patch('/:id', (req, res) => {
-    const { id } = req.params
-    res.status(200).json({
-        "message": 'PATCH /products/:id',
-        "id": id
+router.patch('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const updates = req.body
+        const allowedFields = ['title', 'price', 'imageURL', 'description'];
+        const fieldsToUpdate = Object.keys(req.body);
+
+        if (!fieldsToUpdate.every(field => allowedFields.includes(field))) {
+            const allowedFieldsMessage = allowedFields.join('\n- ');
+            return res.status(400).json({
+                message: `One or more fields are not allowed to update.\nAllowed fields to update:\n- ${allowedFieldsMessage}`
+            });
+        }
+
+        if(!Types.ObjectId.isValid(id))
+            return res.status(404).json({message: "Product not found"})
+
+        const product = await Products.findByIdAndUpdate(
+            id,
+            { $set: updates },
+            { new : true}
+        )
+
+        if(!product)
+            return res.status(404).json({message: "Product not found"})
+        
+        res.status(200).json({
+            "message": 'Product modified successfully',
+            product,
     })
+    } catch(error) {
+        error.status ||= 500
+        error.message = 'Something failed'
+        next(error)
+    }
 })
 
 router.delete('/:id', (req, res) => {
